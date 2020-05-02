c908cdcb1ebe5d68b59a74ddce4ae08c64ada5e3
Carl <bionitsoup@gmail.com>
2020-05-02 15:22:11 +0700

    Adding remarks

--- Assets/ServiceLocator.cs
+++ Assets/ServiceLocator.cs
@@ -12,6 +12,8 @@ using System.Collections.Generic;
 /// </summary>
 /// <remarks>
 /// Unity-specific implementation of a pattern that simplifies IoC and Testing.
+/// Made with ease of use and singleton compatibility in mind for simple dependency graphs.
+/// Underdog alternative to Dependency Injection frameworks, like David vs Goliath.
 /// </remarks>
 public static class ServiceLocator
 {
